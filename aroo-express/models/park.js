89c230d355b9dafc1f2b62b0e997097755c0cc4b
Steve <sbrennison@gmail.com>
2018-08-28 13:03:59 -0500

    api is running

--- aroo-express/models/park.js
+++ aroo-express/models/park.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 
 const parkSchema = new mongoose.Schema({
+  name: String,
   location: String,
   pictures: []
 })
--- aroo-express/server.js
+++ aroo-express/server.js
@@ -13,7 +13,12 @@ require('./db/db')
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
-// const corsOptions
+const corsOptions ={
+  origin: 'http://localhost:3000',
+  credentials: true,
+  optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions))
 
 // const userController = require('./controllers/userController')
 const parkController = require('./controllers/parkController')
